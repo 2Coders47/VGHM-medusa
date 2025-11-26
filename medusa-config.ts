b49b518ecef74dd8a446b7330d166ecec134c5fe
47codes <47codes@proton.me>
2025-11-26 23:47:06 +0100

    Add host and port configuration for Railway

--- medusa-config.ts
+++ medusa-config.ts
@@ -9,14 +9,16 @@ const backendUrl = process.env.BACKEND_URL ||
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    ...(process.env.REDIS_URL && { redisUrl: process.env.REDIS_URL }),
     http: {
+      host: process.env.HOST || "0.0.0.0",
+      port: parseInt(process.env.PORT || "9000"),
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    ...(process.env.REDIS_URL && { redisUrl: process.env.REDIS_URL }),
   },
   admin: {
     backendUrl: backendUrl,
